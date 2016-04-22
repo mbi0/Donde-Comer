d64fe52d12d3ecca90f4186c567115e2489cdf09
mbi0 <marcoelizodo87@gmail.com>
2016-04-22 11:49:42 -0500

    Dondecomer.py

---
+++ Dondecomer.py
@@ -0,0 +1,5 @@
+#Donde Ir a comer cerca del aeropuerto
+import random
+lugares = ['Crown','Carls Jr','Lubbys','Hangar 77','Subway','Taco Express','Ihop','Comedor','Salads','Cabo Grill','Tortas Alex','Cazuelas','Siber Diana']
+print (random.choice(lugares))
+
